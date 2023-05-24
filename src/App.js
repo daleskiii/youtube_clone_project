f4a7fd8b93c8521656418618fe714683fae6687a
daleskiii <aun2793@gmail.com>
2023-05-24 12:52:33 -0400

    fetched api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav/Navbar";
-import About from "./components/about/About"
-import Home from './components/home/Home';
-
+import About from "./components/about/About";
+import Home from "./components/home/Home";
+import Search from "./components/home/Search";
 
 function App() {
   return (
     <BrowserRouter>
-    <Nav/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-    </Routes>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React from "react";
 import "./Search.css";
 
 function Search() {
+  function fetchApi() {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=M7lc1UVf-VE&key=AIzaSyAxrg1FsPQSEGLDOxgF09AhlRbUppuAFys"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the retrieved video data
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the fetch
+        console.log("Error:", error);
+      });
+  }
+
   return (
     <div className="container">
-    <form action="/search" method="GET">
-      <div className="input-group mb-3">
-        <input type="text" name="query" className="form-control" placeholder="Search..."/>
-        <div className="input-group-append">
-          <button type="submit" className="btn btn-danger">Search</button>
+      <form action="/search" method="GET">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            name="query"
+            className="form-control"
+            placeholder="Search..."
+          />
+          <div className="input-group-append">
+            <button
+              type="submit"
+              className="btn btn-danger"
+              onClick={fetchApi()}
+            >
+              Search
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
-  </div>
-  )
+      </form>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
