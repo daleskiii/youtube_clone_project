f9027e40d47ea13a6597961ef4c8b4b5ba97aa24
Chantal <chantalgomez@pursuit.org>
2023-05-24 12:27:43 -0400

    Static page made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav/Navbar";
+import About from "./components/about/About"
+import Home from './components/home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+    <Nav/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/about/About.js
@@ -0,0 +1,9 @@
+export const About = () => {
+    return (
+      <main>
+        <h1>about</h1>
+      </main>
+    );
+  };
+  
+  export default About;
