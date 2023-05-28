5695d4a5928301103ab8530f126bc639e0fb2d2d
Chantal <chantalgomez@pursuit.org>
2023-05-28 16:30:07 -0400

    Video/:id to get to the another screen and play the video is functioning

--- src/components/home/Display.js
+++ src/components/home/Display.js
@@ -1,8 +1,11 @@
 // import React, { useEffect, useState } from 'react'
 import Video from './Video'
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 // import axios from 'axios';
 
 function Display({searchTerm, videoList}) {
+  // const { id } = useParams();
 
 
   return (
@@ -10,7 +13,14 @@ function Display({searchTerm, videoList}) {
        <div className="container text-center">
          <div className="row row-cols-3">
            {videoList.map((item) => {
-            return <Video key={item.id.videoId} {...item} />;
+            // return <Video key={item.id.videoId} {...item} />;
+            return <Link 
+            to={`/video/${item.id.videoId}`}
+            key={item.id.videoId}
+            className="product-link">
+            <Video key={item.id.videoId} {...item} />;
+            </Link>
+
           })}
            </div>
         </div>
