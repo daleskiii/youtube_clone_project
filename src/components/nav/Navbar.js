aaf816a89c449d476900d731864a6dfe88fe531d
daleskiii <aun2793@gmail.com>
2023-05-30 14:32:24 -0400

    attempt-2

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -4,26 +4,36 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg bg-danger">
-    <div className="container-fluid">
-      <Link className="navbar-brand" href="#" to="/">Youtube</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink  className="nav-link active" aria-current="page" to="/" >
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="#" to="/">
+          Youtube
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="true"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
                 Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" href="#" to="/about">
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" href="#" to="/about">
                 About
-                </NavLink>
-          </li>
-        </ul>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
     </nav>
   );
 }
