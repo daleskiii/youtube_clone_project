769e30f32da6a4103780a6ba15de98ef828a6225
daleskiii <aun2793@gmail.com>
2023-05-30 16:59:46 -0400

    fixed image sizes

--- src/components/home/Video.js
+++ src/components/home/Video.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 function Video({ snippet, id }) {
   return (
     <div className="col">
-      <img src={snippet.thumbnails.medium.url} alt={snippet.title} />
+      <img
+        src={snippet.thumbnails.medium.url}
+        alt={snippet.title}
+        style={{ width: "100%", height: "auto" }}
+      />
       <Link to={`/video/${id.videoId}`} style={{ textDecoration: "none" }}>
         <h5 style={{ color: "black", padding: 20 }}>{snippet.title}</h5>
       </Link>
