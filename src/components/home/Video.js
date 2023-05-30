beed60464b2b44128cf52adec0e533015fda45f6
daleskiii <aun2793@gmail.com>
2023-05-30 17:01:51 -0400

    img style fix 2

--- src/components/home/Video.js
+++ src/components/home/Video.js
@@ -7,7 +7,7 @@ function Video({ snippet, id }) {
       <img
         src={snippet.thumbnails.medium.url}
         alt={snippet.title}
-        style={{ width: "100%", height: "auto" }}
+        style={{ width: "100%", height: "100%" }}
       />
       <Link to={`/video/${id.videoId}`} style={{ textDecoration: "none" }}>
         <h5 style={{ color: "black", padding: 20 }}>{snippet.title}</h5>
